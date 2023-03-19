6f31c9c448ad83ee9f28e8c766c4316aed98dedb
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-03-19 18:02:36 +0200

    adding solution

---
+++ AsyncHello/AsyncHello/AsyncReadFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncHello
+{
+   public static class AsyncReadFile
+    {
+        public static async Task<string> ReadHello(string path)
+        {
+            Task<string> task = File.ReadAllTextAsync(path);
+            await task;
+            return task.Result;
+        }
+
+        public static async Task<string> ReadWorld(string path)
+        {
+            Task<string> task = File.ReadAllTextAsync(path);
+            await task;
+            return task.Result;
+        }
+
+        public static async Task<string> Concat(string path1, string path2)
+        {
+            var taskCompletionSource = new TaskCompletionSource();
+            var str1 = await ReadHello(path1);
+            var str2 = await ReadWorld(path2);
+            taskCompletionSource.SetResult();
+            return str1 + " " + str2;
+        }
+    }
+}
---
+++ AsyncHello/AsyncHello/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncHello
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var helloWorld = AsyncReadFile.Concat("Hello.txt", "World.txt");
+            Console.WriteLine(helloWorld.Result);
+        }
+    }
+}
